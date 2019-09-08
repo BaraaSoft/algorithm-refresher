d941b94af2518dc26a381555a5ae99cbabda034c
baraa <baraaabuzeid@hotmail.com>
2019-09-09 01:21:44 +0800

    Breath first search

--- Js/BFS/app.js
+++ Js/BFS/app.js
@@ -5,7 +5,7 @@ const graph = [
     [-1, 1, 0, 1, 0, 0, 0, 0],
     [-1, 1, 1, 0, 0, 1, 0, 0],
     [-1, 1, 0, 0, 0, 1, 0, 0],
-    [-1, 0, 0, 1, 1, 0, 1, 1]
+    [-1, 0, 0, 1, 1, 0, 1, 1],
     [-1, 0, 0, 0, 0, 1, 0, 0],
     [-1, 0, 0, 0, 0, 1, 0, 0],
 ]
@@ -13,28 +13,26 @@ const graph = [
 const algo = (nodenum) => {
 
     let vertixToExplore = [];
-    const visited = [0, 0, 0, 0, 0, 0, 0, 0];
+
+    const visited = [];
     vertixToExplore.push(nodenum)
     visited[nodenum] = 1;
 
     console.log(nodenum);
     while (vertixToExplore.length != 0) {
         let [vertixIndex, ...rest] = vertixToExplore;
-
-
         vertixToExplore = rest;
         for (const [key, vertix] of Object.entries(graph[vertixIndex])) {
-            //console.log("vertix: " + vertix)
-            if ((visited[key] == 0) && vertix == 1) {
+            if ((visited[key] != 1) && vertix == 1) {
                 visited[key] = 1;
                 console.log(key);
                 vertixToExplore.push(key)
             }
         }
-
     }
+
 
 }
 
 
-algo(1);
+algo(3);
