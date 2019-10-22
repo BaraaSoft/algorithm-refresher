5836ed310d903b847fef18bb69f791715c09d4a6
baraabytes <baraaabuzeid@hotmail.com>
2019-10-22 14:05:48 +0800

    insertSort elegent solution

---
+++ Js/sorts/insertSort/main.js
@@ -0,0 +1,16 @@
+// The elegant solution;
+const insertSort = (orgArr) => {
+    for (let i = 1; i < orgArr.length; i++) {
+        let elm = orgArr[i];
+        let j = i - 1;
+        while (j > -1 && elm < orgArr[j]) {
+            orgArr[j + 1] = orgArr[j];
+            j--;
+        }
+        orgArr[j + 1] = elm;
+    }
+    return orgArr;
+}
+
+
+console.log(insertSort([8, 2, 7, 5, 3]))
