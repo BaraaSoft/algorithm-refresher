72ba7979a6b0717fadecbcbefc6c7aed66163599
baraa <baraaabuzeid@hotmail.com>
2020-09-10 20:58:14 +0200

    letter unique detector

---
+++ Js/swiftCodingChallenges/lettersUnique/app.js
@@ -0,0 +1,26 @@
+
+const isUnique = (str, scaned = [], level = 0) => {
+    if (scaned.includes(str[level])) return false
+    else if (level < str.length) return isUnique(str, [...scaned, str[level]], level + 1)
+    else return true
+}
+
+const isUnq = (str = "") => {
+    const scanned = []
+
+    for (const char of str.split('')) {
+
+        if (scanned.includes(char)) return false
+        scanned.push(char)
+    }
+    return true
+}
+
+
+
+const str1 = "Hello"
+const str2 = "AaBbCc"
+
+
+// console.log(isUnique("Hello, world"))
+console.log(isUnq(str1))
