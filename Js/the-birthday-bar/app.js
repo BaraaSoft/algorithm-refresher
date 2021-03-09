61b9242b6cc180296c336f50261598f6e249148e
baraa <baraaabuzeid@hotmail.com>
2021-03-09 21:05:47 +0200

    - the birthday bar problem

--- Js/the-birthday-bar/app.js
+++ Js/the-birthday-bar/app.js
@@ -14,5 +14,34 @@ function birthday(s, d, m) {
         sum(s, l, h) == d ? res++ : 0;
     }
     return res
-
 }
+
+
+// sum = arr.reduce((acc, curr, x) => {
+//     return x <= i ? acc + curr : acc
+// }, 0)
+
+let arr = [3, -9, 1, -30]
+
+
+
+// function maxSubarr(arr) {
+//     let curMax = arr[0], bestMax = arr[0];
+//     for (let i = 1; i < arr.length; i++) {
+//         curMax = Math.max(arr[i] + curMax, arr[i])
+//         if (curMax > bestMax) bestMax = curMax
+//     }
+//     return bestMax;
+// }
+
+function maxSubarr(arr) {
+    let curMax = 0, bestMax = Number.MIN_VALUE;
+    for (let i = 0; i < arr.length; i++) {
+        curMax = Math.max(arr[i] + curMax, arr[i])
+        console.log(`arr[i]: ${arr[i]}, curMax: ${curMax}`)
+        if (curMax > bestMax) bestMax = curMax
+    }
+    return bestMax;
+}
+
+console.log("max subarr: ", maxSubarr(arr))
