55195f3032c8c62dde71ec6c95c5f6055a441c5b
baraa <baraaabuzeid@hotmail.com>
2021-03-24 20:43:46 +0200

    - solve leetcode Best Time to Buy and Sell Stock II

---
+++ leetcode/Sell Stock II/app.js
@@ -0,0 +1,27 @@
+// 122. Best Time to Buy and Sell Stock II
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+
+
+var maxProfit = function (arr) {
+
+    let min = Number.MAX_VALUE;
+    let profit = 0
+
+
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i]
+        } else {
+            profit += (arr[i] - min)
+            min = arr[i]
+        }
+
+    }
+
+    return profit
+};
+
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]))
