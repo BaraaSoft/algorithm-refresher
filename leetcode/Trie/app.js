577fe4a367fc6acbd351fe7e5b79b3e3b3a3f63e
Baraa <baraaabuzeid@hotmail.com>
2021-10-18 21:51:17 +0200

    - implementing trie data structure

---
+++ leetcode/Trie/app.js
@@ -0,0 +1,61 @@
+
+
+var Trie = function () {
+    this.root = new Node()
+};
+
+const Node = function (char) {
+    this.children = {};
+    this.endOfword = false;
+}
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function (word) {
+    let node = this.root;
+    for (let char of word) {
+        if (!(char in node.children))
+            node.children[char] = new Node()
+        node = node.children[char]
+    }
+
+    node.endOfword = true
+
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function (word) {
+    let node = this.root
+    for (let char of word) {
+        if (!(char in node.children)) return false
+        node = node.children[char]
+    }
+
+    return node.endOfword
+};
+
+/** 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function (prefix) {
+    let node = this.root
+    for (let char of prefix) {
+        if (!(char in node.children)) return false
+        node = node.children[char]
+    }
+    return true
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
