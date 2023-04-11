2ccb30bb5c3ce8c1e15b97e3c4270001c8493a98
Baraa <baraaabuzeid@hotmail.com>
2023-04-11 20:27:02 +1000

    sumOfTheLargestSubArray

--- C++/algo/sub-array/main.cpp
+++ C++/algo/sub-array/main.cpp
@@ -2,13 +2,18 @@
 #include<string>
 using namespace std;
 // g++ -std=c++11 -o out main.cpp
-
+int sumOfTheLargestSubArray(int arr[],int len);
 void printAllSubArray(int *arr,int len);
+
+
+
 int main(){
-    int arr[]{1,2,3};
+    int arr[3]{1,2,3};
 
     int len = sizeof(arr)/sizeof(int);
     printAllSubArray(arr,len);
+    int max = sumOfTheLargestSubArray(arr,len);
+    cout<<">> max sub array: "<<max<<endl;
     return 0;
 }
 
@@ -27,5 +32,30 @@ void printAllSubArray(int* arr,int len){
         }
         cout<<endl;
     }
+
+}
+
+
+
+
+int sumOfTheLargestSubArray(int arr[],int len){
+    int maxSum = 0;
+    int sumArr[len];
+    
+    sumArr[0] = arr[0];
+    for(int i = 1;i<len;i++){
+        sumArr[i] = sumArr[i-1]+ arr[i];
+    }
+
+
+    for(int i=0;i<len;i++){
+       
+        for(int j=i;j<len;j++){
+           int sum = i > 0? sumArr[j] - sumArr[i-1]:sumArr[j];
+           maxSum = max(sum,maxSum);
+        }
+    }
+
+return maxSum;
 
 }
