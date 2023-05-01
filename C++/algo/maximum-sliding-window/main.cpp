ff7c90b233f6ad521447932a44091922d33fb4da
Baraa <baraaabuzeid@hotmail.com>
2023-05-01 10:30:32 +1000

    maximum sliding window

--- C++/algo/maximum-sliding-window/main.cpp
+++ C++/algo/maximum-sliding-window/main.cpp
@@ -8,19 +8,46 @@ using namespace std;
 
 vector<int> maximumSlidingWindow(vector<int> arr,int k);
 void printAll(vector<int> arr);
+void slideWindow(vector<int> &arr, vector<int> &window,int i);
 // https://www.youtube.com/watch?v=TCHSXAu5pls
+// https://www.educative.io/courses/grokking-coding-interview-patterns-cpp/YQVXzAAXmjA#Just-the-code
 int main(){
     vector<int> list{1,2,3,4,5,6,7,8,9,10};
+    vector<int> list2{6,1,5,3,9};
     
    
     vector<int> resArr = maximumSlidingWindow(list,4);
+    vector<int> resArr2 = maximumSlidingWindow(list2,3);
     printAll(resArr);
+    printAll(resArr2);
     return 0;
 }
 
+void slideWindow(vector<int> &arr,vector<int> &window,int i){
+
+    while(!window.empty() && arr[window.back()]<= arr[i] ){
+        window.pop_back();
+    }
+
+}
 
 vector<int> maximumSlidingWindow(vector<int> arr, int k){
     vector<int> maxArr{};
+    vector<int> window{};
+
+    for(int i=0; i< k;i++){
+        slideWindow(arr,window,i);
+        window.push_back(i);
+    }
+    maxArr.push_back(arr[window.front()]);
+    for(int i=k;i<arr.size();i++){
+        slideWindow(arr,window,i);
+        if(!window.empty() && window.front() <= i-k){
+            window.erase(window.begin());
+        }
+         window.push_back(i);
+         maxArr.push_back(arr[window.front()]);
+    }
    
     return maxArr;
 }
@@ -29,4 +56,5 @@ void printAll(vector<int> arr){
     for(auto  &elm :arr){
         cout<<elm<<endl;
     }
+    cout<<"----"<<endl;
 }
