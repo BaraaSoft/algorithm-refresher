3df53c1db862abcd13638511c5e45f5acb371031
BaraaSoft <baraaabuzeid@hotmail.com>
2024-07-17 02:33:23 +1000

    minimum window substring

--- Grokking/algo2024/slidingWindow/miniWindowSubstring/main.cpp
+++ Grokking/algo2024/slidingWindow/miniWindowSubstring/main.cpp
@@ -31,6 +31,51 @@ string minWindow(string s, string t){
     map<char,int> charFreqT{};
     map<char,int> window{};
     for(auto ch= t.begin(); ch != t.end();ch++ ){
+         charFreqT[*ch] +=1;
+    }
+
+    int currMatchSize = 0;
+    int requiredMatchSize = charFreqT.size();
+
+    int l=0;
+    pair<int,int> res = make_pair(0,0);
+    int minStrLen= INT_MAX;
+
+    for(int r=0; r<s.length();r++){
+        char ch = s[r];
+        window[ch] += 1;
+       
+        if(charFreqT.find(ch) != charFreqT.end()){
+            if(window.at(ch) == charFreqT.at(ch)){
+                currMatchSize++;
+            }
+        }
+        
+        while (currMatchSize == requiredMatchSize && l <= r)
+        {
+            char lchar = s[l];
+            if( r-l+1 < minStrLen){
+                minStrLen = r-l+1;
+                res = make_pair(l,r);
+            }
+            
+            window[lchar] -=1;
+            if(charFreqT.find(lchar) != charFreqT.end()){
+                if(window[lchar] < charFreqT[lchar]) currMatchSize--;
+            }
+            l++;
+        }
+        
+    }
+
+    return minStrLen == INT_MAX?"": s.substr(res.first,res.second - res.first +1); 
+}
+
+
+string minWindow2(string s, string t){
+    map<char,int> charFreqT{};
+    map<char,int> window{};
+    for(auto ch= t.begin(); ch != t.end();ch++ ){
         if(charFreqT.find(*ch) == charFreqT.end()){
             charFreqT[*ch]=0;
             window[*ch]=0;
