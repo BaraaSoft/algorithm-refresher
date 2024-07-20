ccd5195598043d14a3071ba5b31776b7b8f1d97d
BaraaSoft <baraaabuzeid@hotmail.com>
2024-07-21 00:16:59 +1000

    medianSlidingWindow

--- Grokking/algo2024/twoHeaps/slidingWindowMedian/main.cpp
+++ Grokking/algo2024/twoHeaps/slidingWindowMedian/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<queue>
 #include<vector>
+#include<map>
 
 using namespace std;
 // g++ -std=c++11 -o out main.cpp
@@ -10,6 +11,13 @@ vector<double> medianSlidingWindow(vector<int> nums, int k);
 
 int main(){
 
+    vector<int> list{1,3,-1,2,-2,-3,5,1,5,3};
+
+    auto results = medianSlidingWindow(list,4);
+
+    for(auto it: results){
+        cout<<it<<","<<endl;
+    }
 
     return 0;
 }
@@ -17,8 +25,18 @@ int main(){
 
 vector<double> medianSlidingWindow(vector<int> nums, int k)
 {
-    // Replace this placeholder return statement with your code
 
-    return {0.0};
+    vector<double> results{};
+    for(int i=0;i+k<=nums.size();i++){
+        vector<double> window(nums.begin()+i,nums.begin()+i+k);
+        sort(window.begin(),window.end());
+        if(k & 1){
+            results.push_back(window[k/2]);
+        }else{
+            double median = (window[k/2-1]+window[k/2])*0.5;
+            results.push_back(median);
+        }
+    }
+    return results;
 
 }
