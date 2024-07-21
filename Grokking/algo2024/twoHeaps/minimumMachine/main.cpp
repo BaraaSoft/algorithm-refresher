17420ecec92c5fcc28c56b502d842eb3fba5fff1
BaraaSoft <baraaabuzeid@hotmail.com>
2024-07-22 09:17:02 +1000

    minimumMachine

---
+++ Grokking/algo2024/twoHeaps/minimumMachine/main.cpp
@@ -0,0 +1,72 @@
+#include<queue>
+#include<vector>
+#include<utility>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+// g++ -std=c++11 -o out main.cpp
+
+int tasksFun(vector<vector<int>> tasksList);
+
+int main(){
+
+    vector<vector<int>> taskslist = {{1,1},{5,5},{8,8},{4,4},{6,6},{10,10},{7,7}};
+    vector<vector<int>> taskslist2{ {1, 7}, {8, 9}, {3, 6}, {9, 14}, {6, 7}};
+
+    int result = tasksFun(taskslist);
+    int result2 = tasksFun(taskslist2);
+
+
+    cout<<">> 1. machines: "<< result <<endl;
+    cout<<">> 2. machines: "<< result2 <<endl;
+
+
+    return 0;
+}
+
+
+struct pairGreaterSecond{
+    constexpr bool operator()(pair<int,int> const &a, pair<int,int> const &b)const noexcept{ return a.second > b.second;};
+};
+
+
+// [(1, 7)x, (8, 9)x, (3, 6)x, (9, 14), (6, 7)x]
+// {{1,7}, {3,6}, {6,7}, {8,9}, {9,14}}
+// 
+
+int tasksFun(vector<vector<int>> tasksList)
+{
+
+    priority_queue<pair<int,int>, vector<pair<int,int>>,pairGreaterSecond> minHeap;
+
+    for(auto item:tasksList){
+        minHeap.push(make_pair(item[0],item[1]));
+    }
+
+    pair<int,int> item = minHeap.top();
+    minHeap.pop();
+
+
+    int start= item.first;
+    int end = item.second;
+
+    int machineCount=1;
+
+    while(!minHeap.empty()){
+        pair<int,int> curr = minHeap.top();
+
+        if(curr.first < end){
+            machineCount++;
+        }else{}
+
+        start = std::max(curr.first,start);
+        end = std::max(curr.second,end);
+
+        minHeap.pop();
+
+    }
+
+
+
+    return machineCount;
+}
