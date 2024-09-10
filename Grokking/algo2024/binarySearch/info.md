d52700c9709612e185c869f006812d2acc0cb164
Baraa <baraaabuzeid@hotmail.com>
2024-09-11 01:32:19 +1000

    time base key value store

---
+++ Grokking/algo2024/binarySearch/info.md
@@ -0,0 +1,5 @@
+## More questions
+
+### Binary Search
+
+https://www.youtube.com/watch?v=fu2cD_6E8Hw&list=PLot-Xpze53leNZQd0iINpD-MAhMOMzWvO&index=8
---
+++ Grokking/algo2024/binarySearch/timeBaseKeyValueStore/index.js
@@ -0,0 +1,56 @@
+// Q) https://leetcode.com/problems/time-based-key-value-store/
+
+var TimeMap = function () {
+  this.store = new Map();
+};
+
+/**
+ * @param {string} key
+ * @param {string} value
+ * @param {number} timestamp
+ * @return {void}
+ */
+TimeMap.prototype.set = function (key, value, timestamp) {
+  if (this.store.has(key)) {
+    let node = this.store.get(key);
+    node.push({ timestamp, value });
+  } else {
+    this.store.set(key, [{ timestamp, value }]);
+  }
+};
+
+/**
+ * @param {string} key
+ * @param {number} timestamp
+ * @return {string}
+ */
+TimeMap.prototype.get = function (key, timestamp) {
+  let nodeArr = this.store.get(key);
+  if (!nodeArr) return '';
+  let index = this.search(timestamp, nodeArr);
+  if (index <= -1) return '';
+  return nodeArr[index]?.value;
+};
+
+TimeMap.prototype.search = function (target, arr) {
+  let start = 0,
+    end = arr.length;
+
+  while (start < end) {
+    const mid = Math.floor((start + end) / 2);
+    const { timestamp } = arr[mid];
+    if (target >= timestamp) {
+      start = mid + 1;
+    } else {
+      end = mid;
+    }
+  }
+  return end - 1;
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * var obj = new TimeMap()
+ * obj.set(key,value,timestamp)
+ * var param_2 = obj.get(key,timestamp)
+ */
