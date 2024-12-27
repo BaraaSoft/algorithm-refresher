d7f729ec8cd2d28aff75cafa0f1fd83534e91f10
BaraaSoft <baraaabuzeid@hotmail.com>
2024-12-27 14:49:31 +1100

    ReverseWordInString

--- Java/java-eco/algorithms/src/main/java/com/baraabytes/twoPointers/ReverseWordInString.java
+++ Java/java-eco/algorithms/src/main/java/com/baraabytes/twoPointers/ReverseWordInString.java
@@ -1,12 +1,29 @@
 package com.baraabytes.twoPointers;
 
+import java.util.Arrays;
+
 public class ReverseWordInString {
 
     public String reverseWords(String s) {
+       String[] strArr =  s.split(" ");
+       String[] arr = Arrays.stream(strArr).filter(x -> !x.isEmpty()).toArray(String[]::new);
+       return  reverse(arr,0).trim();
+    }
 
+
+    public String reverse(String[] arr,int index){
+        if(index == arr.length) return "";
+
+        String str = reverse(arr,index+1) +" "+arr[index];
+
+        return str;
     }
 
     public static void main(String[] args) {
         ReverseWordInString reverseWordInString = new ReverseWordInString();
+
+        System.out.println(reverseWordInString.reverseWords("Reverse this string"));
+        System.out.println(reverseWordInString.reverseWords("The quick brown fox jumped over a lazy dog"));
+
     }
 }
