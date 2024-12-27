73c3cfc31d963f60a207e27db69b5214baa7573c
BaraaSoft <baraaabuzeid@hotmail.com>
2024-12-27 14:20:14 +1100

    update

---
+++ Java/java-eco/algorithms/src/main/java/com/baraabytes/twoPointers/ReverseWordInString.java
@@ -0,0 +1,12 @@
+package com.baraabytes.twoPointers;
+
+public class ReverseWordInString {
+
+    public String reverseWords(String s) {
+
+    }
+
+    public static void main(String[] args) {
+        ReverseWordInString reverseWordInString = new ReverseWordInString();
+    }
+}
--- Java/java-eco/algorithms/src/main/java/com/baraabytes/twoPointers/XSortColors.java
+++ Java/java-eco/algorithms/src/main/java/com/baraabytes/twoPointers/XSortColors.java
@@ -1,10 +1,10 @@
 package com.baraabytes.twoPointers;
 
-final public class SortColors {
+final public class XSortColors {
 
     public void sortColors(int[] nums) {
         int start =0,end=nums.length-1;
-        int curr =1;
+        int curr =0;
         while(curr <= end){
             if(nums[curr]==0){
                 int temp = nums[curr];
