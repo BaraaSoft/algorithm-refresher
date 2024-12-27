f05168f5141ae63dcb95ed9a4faa2791f763f12c
BaraaSoft <baraaabuzeid@hotmail.com>
2024-12-27 17:32:53 +1100

    reverse word

--- Java/java-eco/algorithms/src/main/java/com/baraabytes/twoPointers/ReverseWordInString.java
+++ Java/java-eco/algorithms/src/main/java/com/baraabytes/twoPointers/ReverseWordInString.java
@@ -1,5 +1,6 @@
 package com.baraabytes.twoPointers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ReverseWordInString {
@@ -19,11 +20,48 @@ public class ReverseWordInString {
         return str;
     }
 
+
+    public String reverse(String s,int start,int end){
+        char[] str = s.toCharArray();
+        while(start < end){
+            char temp = str[start];
+            str[start++]= str[--end];
+            str[end]= temp;
+        }
+        return String.copyValueOf(str);
+    }
+    public void reverse(char[] str,int start,int end){
+        while(start < end){
+            char temp = str[start];
+            str[start++]= str[--end];
+            str[end]= temp;
+        }
+    }
+
+    public String reverseWord(String s){
+        char[] str = s.toCharArray();
+        reverse(str,0,str.length);
+        int start = 0,end=0;
+
+        while(end <= str.length){
+            if( end == str.length || str[end] == ' ' ){
+                reverse(str,start,end);
+                end++;
+                start = end;
+            }else{
+                end++;
+            }
+        }
+        return String.copyValueOf(str);
+    }
+
     public static void main(String[] args) {
         ReverseWordInString reverseWordInString = new ReverseWordInString();
 
         System.out.println(reverseWordInString.reverseWords("Reverse this string"));
         System.out.println(reverseWordInString.reverseWords("The quick brown fox jumped over a lazy dog"));
+
+        System.out.println(reverseWordInString.reverseWord("Reverse this string"));
 
     }
 }
