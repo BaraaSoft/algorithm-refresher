4f53d39dcc9edf9857de0d5470369cc6bc26cd94
BaraaSoft <baraaabuzeid@hotmail.com>
2025-01-07 07:43:00 +1100

    GraphValidTree

---
+++ Java/java-eco/algorithms/src/main/java/com/baraabytes/graph/GraphValidTree.java
@@ -0,0 +1,19 @@
+package com.baraabytes.graph;
+
+public class GraphValidTree {
+
+    public static void main (String[] args){
+
+        GraphValidTree graphValidTree = new GraphValidTree();
+
+        System.out.println();
+
+    }
+
+
+    public boolean validTree(int n, int[][] edges) {
+
+        // Replace this placeholder return statement with your code
+        return false;
+    }
+}
