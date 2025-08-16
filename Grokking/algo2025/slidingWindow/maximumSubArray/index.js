b3c50f4affef4aeb11fb966d5f8ece5008828672
baraa <baraaabuzeid@hotmail.com>
2025-08-16 20:41:54 +1000

    maximum subarray

---
+++ Grokking/algo2025/slidingWindow/maximumSubArray/index.js
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/maximum-subarray/
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+    let maxSum = nums[0];
+    let currSum = nums[0]
+
+
+    for (let i = 1; i < nums.length; i++) {
+        if (currSum < 0) currSum = 0;
+
+        currSum += nums[i];
+        maxSum = Math.max(maxSum, currSum)
+    }
+    return maxSum;
+
+};
